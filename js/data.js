f5f0813a77200bd3403442e71e599dacbfe050aa
Alexander Scheurer <mail@aspepex.net>
2021-05-20 20:40:11 +0200

    Fill

---
+++ js/data.js
@@ -0,0 +1,38 @@
+var headerText = "Longship Subnautica Bingo";
+
+var footerText = "<a href='https://forms.gle/kRzYoGoH67Hz3BPL8' target='_blank'>Suggest Tiles</a> - <a href='https://github.com/LongshipStudios/bingo/' target='_blank'>Code available on github</a>";
+
+var winText = "Winner";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/Sfx_creature_shadowleviathan_roar_aggro_01.ogg");
+
+var JSONBingo = {
+    "squares": [
+        { "square": "*Will screams in fear*" },
+        { "square": "*Will screams in terror*" },
+        { "square": "Seamonkeys are so cute!" },
+        { "square": "*Will is stalling*" },
+        { "square": "AAAaaaaaahhhh!" },
+        { "square": "*Will is scared by not a monster*" },
+        { "square": "NOOOOOOOO!!!" },
+        { "square": "Nonoonono!!!" },
+        { "square": "*Will swims past something he needs*" },
+        { "square": "Just breath" },
+        { "square": "*Fiona scares Will*" },
+        { "square": "Penglings are so cute!" },
+        { "square": "AAAAArrasasghasd!" },
+        { "square": "*Will tries to hide IRL*" },
+        { "square": "Stop stealing my stuff!" },
+        { "square": "Have we been here?" },
+        { "square": "*Will is stalling some more*" },
+        { "square": "*Will is stalling even more*" },
+        { "square": "This is my biggest fear!" },
+        { "square": "I love this game" },
+        { "square": "This is so beautiful" },
+        { "square": "EAT MY BUTT!" },
+        { "square": "Fecking WEEEEEE!" },
+        { "square": "*Visiting penguin island, even tho there is nothing to do there*" }
+    ]
+};
